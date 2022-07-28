0d8cddb20bbcd51290834717fcac13ba4cdd38b6
TannerEsch <eschmanntanner@gmail.com>
2022-07-27 21:34:18 -0400

    add: first push of project 1

---
+++ app.js
@@ -0,0 +1,8 @@
+const pipes = document.getElementsByClassName("pipe");
+const havoc = document.querySelector(".havoc");
+
+document.addEventListener("click", e => {
+       console.log(e.target) 
+})
+   
+   
