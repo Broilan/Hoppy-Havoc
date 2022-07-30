4b0932e1369fb7c3d4fd2e87a6bee04cfff9de92
TannerEsch <eschmanntanner@gmail.com>
2022-07-30 14:02:45 -0400

    add: making a new method for pipes/pipe gravity, commiting before deleting old code

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const havoc = document.querySelector(".havoc");
-const pipe1 = document.getElementById("pipe1");
-const pipe2 = document.getElementById("pipe2");
-const pipe3 = document.getElementById("pipe3");
-const pipe4 = document.getElementById("pipe4");
-const pipe5 = document.getElementById("pipe5");
+const display = document.querySelector('.container')
+// const pipe1 = document.getElementById("pipe1");
+// const pipe2 = document.getElementById("pipe2");
+// const pipe3 = document.getElementById("pipe3");
+// const pipe4 = document.getElementById("pipe4");
+// const pipe5 = document.getElementById("pipe5");
 
-
-const keyPress = 20;
-const pipeGravity = 1;
+//Character Movement Constant + gravity
+const keyPress = 250;
 const charLilG = 1;
+
+//randomizing pipe movement
+//neg
+// const pipe1G = Math.floor(Math.random() * 100 + 25);
+// const pipe2G = Math.floor(Math.random() * 100 + 25);
+// const pipe3G = Math.floor(Math.random() * 100 + 25);
+// const pipe4G = Math.floor(Math.random() * 100 + 25);
+// const pipe5G = Math.floor(Math.random() * 100 + 25);
+// //pos
+// const pipe1Gr = Math.floor(Math.random() * 100 + 25);
+// const pipe2Gr = Math.floor(Math.random() * 100 + 25);
+// const pipe3Gr = Math.floor(Math.random() * 100 + 25);
+// const pipe4Gr = Math.floor(Math.random() * 100 + 25);
+// const pipe5Gr = Math.floor(Math.random() * 100 + 25);
+
+//Page sizing
+let pageWidth = 1297;
+let pageHeight = 985;
 
 
 // character movement
@@ -40,38 +58,75 @@ function charGravity() {
     havoc.style.top = Movement;
 };
 
+//creating pipes
+function generateObstacles() {
+    const obstacle = document.createElement('div');
+    obstacle.classList.add('obstacle');
+    display.appendChild(obstacle);
+}
+generateObstacles();
+
 //pipeGravity in the negative y direction
-function pipe1Gravity() {
-    let movementPipes = parseInt(pipe1.style.top) + pipeGravity + 'px';
-    pipe1.style.top = movementPipes;
-};
-function pipe2Gravity() {
-    let movementPipes = parseInt(pipe2.style.top) + pipeGravity + 'px';
-    pipe2.style.top = movementPipes;
-};
-function pipe3Gravity() {
-    let movementPipes = parseInt(pipe3.style.top) + pipeGravity + 'px';
-    pipe3.style.top = movementPipes;
-};
-function pipe4Gravity() {
-    let movementPipes = parseInt(pipe4.style.top) + pipeGravity + 'px';
-    pipe4.style.top = movementPipes;
-};
-function pipe5Gravity() {
-    let movementPipes = parseInt(pipe5.style.top) + pipeGravity + 'px';
-    pipe5.style.top = movementPipes;
-};
+// function pipe1Gravity() {
+//     let movementPipes = parseInt(pipe1.style.top) + pipe1G + 'px';
+//     pipe1.style.top = movementPipes;
+// };
+// function pipe2Gravity() {
+//     let movementPipes = parseInt(pipe2.style.top) + pipe2G + 'px';
+//     pipe2.style.top = movementPipes;
+// };
+// function pipe3Gravity() {
+//     let movementPipes = parseInt(pipe3.style.top) + pipe3G + 'px';
+//     pipe3.style.top = movementPipes;
+// };
+// function pipe4Gravity() {
+//     let movementPipes = parseInt(pipe4.style.top) + pipe4G + 'px';
+//     pipe4.style.top = movementPipes;
+// };
+// function pipe5Gravity() {
+//     let movementPipes = parseInt(pipe5.style.top) + pipe5G + 'px';
+//     pipe5.style.top = movementPipes;
+// };
 
 //Pipe Gravity in the positive y direction
+// function posPipe1Gravity() {
+//     let movementPipes = parseInt(pipe1.style.top) - pipe1Gr + 'px';
+//     pipe1.style.top = movementPipes;
+// };
+// function posPipe2Gravity() {
+//     let movementPipes = parseInt(pipe2.style.top) - pipe2Gr + 'px';
+//     pipe2.style.top = movementPipes;
+// };
+// function posPipe3Gravity() {
+//     let movementPipes = parseInt(pipe3.style.top) - pipe3Gr + 'px';
+//     pipe3.style.top = movementPipes;
+// };
+// function posPipe4Gravity() {
+//     let movementPipes = parseInt(pipe4.style.top) - pipe4Gr + 'px';
+//     pipe4.style.top = movementPipes;
+// };
+// function posPipe5Gravity() {
+//     let movementPipes = parseInt(pipe5.style.top) - pipe5Gr + 'px';
+//     pipe5.style.top = movementPipes;
+// };
 
-//Gravity times
-setInterval(pipe1Gravity, 1);
-setInterval(pipe2Gravity, 1);
-setInterval(pipe3Gravity, 1);
-setInterval(pipe4Gravity, 1);
-setInterval(pipe5Gravity, 1);
+// creating page boundaries
+
+
+//Negative y direction gravity times
+// setInterval(pipe1Gravity, 1500);
+// setInterval(pipe2Gravity, 1500);
+// setInterval(pipe3Gravity, 1500);
+// setInterval(pipe4Gravity, 1500);
+// setInterval(pipe5Gravity, 1500);
 setInterval(charGravity, 5);
 
+//postive y direction gravity times
+// setInterval(posPipe1Gravity, 1000);
+// setInterval(posPipe2Gravity, 1000);
+// setInterval(posPipe3Gravity, 1000);
+// setInterval(posPipe4Gravity, 1000);
+// setInterval(posPipe5Gravity, 1000);
 
 
 
