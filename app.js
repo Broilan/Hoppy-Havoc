fd35fe35c6cf620d26eb2c2ded5db3d1fba5de41
TannerEsch <eschmanntanner@gmail.com>
2022-08-03 21:33:52 -0400

    add: game functional, just adding some scoring, end screen, and start screen css

--- app.js
+++ app.js
@@ -25,14 +25,41 @@ let arrows = window.addEventListener('keyup', (e) => {
     }
 });
 
+//score tracking
+function score() {
+    let scoreX = document.createElement('h1');
+    scoreX.classList.add('score');
+    scoreX.textContent = "score:" + counter;
+    scoreX.style.display = 'flex';
+    scoreX.style.justifyContent = 'center';
+    scoreX.style.alignItems = 'center';
+    scoreX.style.top = '-22px';
+    scoreX.style.left = '45%';
+    scoreX.style.height = '75.3px';
+    scoreX.style.width = '150px';
+    scoreX.style.backgroundColor = 'lightBlue';
+    body.appendChild(scoreX);
+}
+score();
 //game start
+let startScreen = document.createElement('div');
+startScreen.classList.add('startScreen')
+startScreen.style.height = '500px';
+startScreen.style.width = '800px';
+startScreen.style.borderRadius = '100px', 'black';
+startScreen.style.backgroundColor = 'lightBlue';
+startScreen.style.top = '220px';
+startScreen.style.left = '530px';
+body.appendChild(startScreen);
+
 let gameOn = false
 window.addEventListener('keyup', (e) =>{
 switch(e.key) {
     case 'Enter':
        gameOn = true;
+       body.removeChild(startScreen);
 setInterval(charGravity, 20);
-goInterval = setInterval(generateObstacles, 5000);
+goInterval = setInterval(generateObstacles, 1750);
 }
 })
 
@@ -48,7 +75,7 @@ function charGravity() {
 function generateObstacles() {
     let obstacle = document.createElement('div');
     obstacle.classList.add('obstacle');
-    obstacle.style.left = '1320px';
+    obstacle.style.left = '1900px';
     let randomNum = Math.floor(Math.random() * window.innerHeight) - 250;
     obstacle.style.top = parseInt(randomNum) + 'px';
     display.appendChild(obstacle);
@@ -98,9 +125,19 @@ function gameOver() {
     body.appendChild(gameOverText);
     gameOverText.style.top = '450px';
     gameOverText.style.left = '657px';
-return;
+    let reset = document.createElement('button');
+    reset.classList.add('reset');
+    reset.textContent = 'Restart';
+    body.appendChild(reset);
+    reset.style.top = '650px';
+    reset.style.left = '657px';
+    reset.addEventListener('click', (e) => {
+        window.location.reload();
+    })
+} 
+
 }
-}
+
 
    
 
